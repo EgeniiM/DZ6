d94c86a96e6ea46f15234bc0333983aa36a62451
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-06-02 21:11:59 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allImages = 52;
+            int rawImages = 3;
+            int allRaw = (allImages / rawImages);
+            int outImages = (allImages - allRaw * rawImages);
+            Console.WriteLine("Рядов с картинками: {0} оставшиеся картинки: {1}",allRaw , outImages);
+        }
+    }
+}
